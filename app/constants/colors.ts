163092059204672b5ae9aea92b6b1cab56a5b4f2
hazel <joyceminz58@gmail.com>
2026-04-12 19:12:19 +0530

    finished setting up

---
+++ app/constants/colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  severeDecrease: '#A50026',
+  moderateDecrease: '#F46D43',
+  noChange: '#FFFFBF',
+  moderateIncrease: '#74C476',
+  severeIncrease: '#006837',
+  primary: '#2E7D32',       // App green
+  background: '#F5F5F5',
+  white: '#FFFFFF',
+  text: '#212121',
+  subtext: '#757575',
+};
